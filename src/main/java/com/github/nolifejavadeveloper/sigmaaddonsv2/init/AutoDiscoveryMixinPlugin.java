4c15e00843f96e1ffce7e8a971ed8c4ae57bdd8a
GitHub Action <action@github.com>
2025-02-04 01:14:26 +0000

    Initialize template repository proper
    Set package name to com.github.nolifejavadeveloper.sigmaaddonsv2
    Set project name to SigmaAddonsv2
    Set modid to sigmaaddonsv

--- src/main/java/com/github/nolifejavadeveloper/sigmaaddonsv2/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/nolifejavadeveloper/sigmaaddonsv2/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.nolifejavadeveloper.sigmaaddonsv2.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
