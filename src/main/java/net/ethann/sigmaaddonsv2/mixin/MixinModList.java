c0b0d0b3a0a0a12d2c05b034d7260f15ab6180b9
Ethan <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-02-04 22:45:08 -0500

    added modid hider

---
+++ src/main/java/net/ethann/sigmaaddonsv2/mixin/MixinModList.java
@@ -0,0 +1,29 @@
+package net.ethann.sigmaaddonsv2.mixin;
+
+import net.ethann.sigmaaddonsv2.SigmaAddons;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.ModContainer;
+import net.minecraftforge.fml.common.network.handshake.FMLHandshakeMessage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.Map;
+
+@Mixin(value = FMLHandshakeMessage.ModList.class, remap = false)
+public class MixinModList {
+
+    @Shadow
+    private Map<String, String> modTags;
+
+    @Inject(method = "<init>(Ljava/util/List;)V", at = @At("RETURN"))
+    private void init(List<ModContainer> modList, CallbackInfo ci) {
+        if (!Minecraft.getMinecraft().isIntegratedServerRunning()) {
+            this.modTags.keySet().removeIf(c -> c.equals(SigmaAddons.MOD_ID));
+        }
+    }
+}
