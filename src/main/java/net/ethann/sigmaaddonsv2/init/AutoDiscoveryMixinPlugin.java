55369d3f339994da9a43fa14a3ae80238146e05e
Ethan <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-02-04 22:46:15 -0500

    changed the package

--- src/main/java/net/ethann/sigmaaddonsv2/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/net/ethann/sigmaaddonsv2/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.github.nolifejavadeveloper.sigmaaddonsv2.init;
+package net.ethann.sigmaaddonsv2.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
