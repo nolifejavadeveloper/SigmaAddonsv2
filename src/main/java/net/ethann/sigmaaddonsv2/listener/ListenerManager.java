34722cdb7f65658538e90204f6384711c3cf32fc
Ethan <122495136+nolifejavadeveloper@users.noreply.github.com>
2025-02-12 21:48:31 -0500

    added ListenerManager to register listeners better

---
+++ src/main/java/net/ethann/sigmaaddonsv2/listener/ListenerManager.java
@@ -0,0 +1,18 @@
+package net.ethann.sigmaaddonsv2.listener;
+
+import net.minecraftforge.common.MinecraftForge;
+import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
+
+import java.util.Set;
+
+public class ListenerManager {
+    public void init() throws InstantiationException, IllegalAccessException {
+        Reflections reflections = new Reflections("net.ethann.sigmaaddonsv2.listener.impl");
+        Set<Class<?>> classes = reflections.get(Scanners.SubTypes.of(Object.class).asClass());
+
+        for (Class<?> clazz : classes) {
+            MinecraftForge.EVENT_BUS.register(clazz.newInstance());
+        }
+    }
+}
